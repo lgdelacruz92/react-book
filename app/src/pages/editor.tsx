591f69e1d99c616a6688e71a97cbcf567fbb3acb
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro-3.local>
2023-04-20 12:21:12 -0400

    dded editor

--- app/src/pages/editor.tsx
+++ app/src/pages/editor.tsx
@@ -2,9 +2,14 @@ import { useState, useEffect, useRef } from "react";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { schema } from "prosemirror-schema-basic";
-import { addListNodes } from "prosemirror-schema-list";
+import { exampleSetup } from "prosemirror-example-setup";
 import { Schema } from "prosemirror-model";
+import "prosemirror-menu/style/menu.css";
 
+import { addListNodes } from "prosemirror-schema-list";
+
+// Mix the nodes from prosemirror-schema-list into the basic schema to
+// create a schema with list support.
 const mySchema = new Schema({
   nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
   marks: schema.spec.marks,
@@ -21,6 +26,7 @@ function Editor() {
 
     const state = EditorState.create({
       schema: mySchema,
+      plugins: exampleSetup({ schema: mySchema }),
     });
     const view = new EditorView(editorRef.current, {
       state,
